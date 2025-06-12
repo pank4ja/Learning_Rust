51a33dc9aa2a5cfb432a49501fdf70f0ad0aea10
Pankaj Agarwal <drawishu@gmail.com>
2025-06-12 12:18:37 +0530

    Mutability Done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
+// const red: u8 = 10;
+
 fn main() {
-    println!("Hello, world!");
+    // let age: u8 = 22;
+    // print!("Age : {age}");
+
+    // age = 24;
+    // println!("value of age is: {age}");  // compile time error
+
+    // let mut score = 03;
+    // score ="playground";  // can't change the type of variable with mut 
+    // score = 14;
+    // println!("now the score is : {score}");
+
+    // const PI: f32 = 3.14;
+    // println!("value of constant is : {PI}");
+
+    // const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;  // yeh compile time determine kar sakta hai 
+
+    // let a = 19;
+    // const B: u8 = 1 + a;  // a non constant value is used it will give error
+
+    // const c: u8 = 9;
+    // const d : u8 = 1 + c;  // can use const express 
+
+    // let apple = 10;
+
+    // let apple = true;
+
+    // println!("apple {apple}")
+
+    // let orange = 10;
+
+    // let orange = orange + 10;
+    // println!("{orange}");
+
+
+    // let x = 5;
+
+    // let x = x + 1;
+
+    // {
+    //     let x = x * 2;
+    //     println!("The value of x in the inner scope is: {x}");
+    // }
+
+    // println!("The value of x is: {x}");
+
+
+    // let mut age = 24;
+    // age = age + 10;
+    // let age = age;
+    // age = age +1; // not possible as it became immutable
 }
